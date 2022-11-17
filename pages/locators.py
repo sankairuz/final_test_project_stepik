9d95ca1ea38152d73787ef62683eefbcd4f00d5b
sankairuz <sankairuz@gmail.com>
2022-11-17 12:49:41 +0300

    Создание класса с методами проверки корректности страницы логина/регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocator():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocator():
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
