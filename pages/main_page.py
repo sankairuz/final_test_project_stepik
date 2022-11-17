e9c7eb4e5cfb5b2686ae1cdf2d7ac38c527e1a0d
sankairuz <sankairuz@gmail.com>
2022-11-17 12:20:40 +0300

    локаторы берутся кортежами из класса с переменными

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,14 @@
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocator
 
 from .base_page import BasePage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocator.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocator.LOGIN_LINK), "Login link is not presented"
 
