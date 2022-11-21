8485753098f60f9073c92f7dd14846a59e49f1ab
sankairuz <sankairuz@gmail.com>
2022-11-21 12:39:25 +0300

    Добавлена проверка страницы с логином

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,11 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):  # BasePage в скобках указывает на то, что MainPage - его наследник
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, '#login_link')
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        self.is_element_present(By.CSS_SELECTOR, '#login_link'), "Login is not presented"
+        self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login is not presented"
