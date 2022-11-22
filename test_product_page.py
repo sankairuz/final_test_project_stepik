482167992c55e7cf12bf4ced5f9c1737aea3d394
sankairuz <sankairuz@gmail.com>
2022-11-22 10:37:26 +0300

    добавлениие Xfail к падающему тесту

--- test_product_page.py
+++ test_product_page.py
@@ -10,10 +10,10 @@ import pytest
 #     page.add_to_basket()
 #     page.solve_quiz_and_get_code()
 #     page.verify_price_name()
-@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", "7", "8", "9"])
+@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-    browser.delete_all_cookies()
+    browser.delete_all_cookies()  #очищает кукис перед новым тестом
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
